cf99f6a677f0a5ccce65cef11648dcd1c7d4b4c1
Vojtech Bocek <vbocek@gmail.com>
2015-03-05 16:51:25 +0100

    MultiRom: Implement kexec-hardboot: Re Ported to work on New Caf/MSM8974 Kernels (CM13)
    Implement kexec-hardboot: Re Ported to work on New Caf/MSM8974 Kernels (CM13)
    
    "Allows hard booting (i.e., with a full hardware reboot) to a kernel
    previously loaded in memory by kexec.  This works around the problem of
    soft-booted kernel hangs due to improper device shutdown and/or
    reinitialization."
    More info in /arch/arm/Kconfig.
    
    Original author: Mike Kasick <mike@kasick.org>
    
    Eliminater74 <eliminater74@gmail.com>:
      I've ported it to LG G3, it is based of my hammerhead, mako, flo
      and grouper ports, which are based of Asus TF201 patch ported
      by Jens Andersen <jens.andersen@gmail.com>
    
      G3 is rather similar to hammerhead - it uses device tree binary instead
      of atags. It is also rather picky - its bootloader erases first 128MB of RAM
      on reboot, so following hacks had to be applied:
      * Copy dtb to location passed in r2 before jumping to kexecd' kernel,
        the kernel would just freeze when I left dtb above 128MB of RAM.
      * Copy kexecd' kernel to location in the first 128MB before jumping to it,
        the decompressor operates very slowly if the kernel is above the cached
        area, copying it lower and decompressing it afterwards is much faster.
    
      The guest kernel does not require any modifications, kexec-hardboot
      is equivalent to standard kexec in this aspect.
    
    ayysir <dresadd09691@gmail.com>:
      kexec: use mem_text_write_kernel_word to set reboot_code_buffer args
      in order to avoid protection faults (writes to read-only
      kernel memory) when CONFIG_STRICT_MEMORY_RWX is enabled.
    
    Signed-off-by: Vojtech Bocek <vbocek@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>
    
    MultiRom: Kexec Hardboot Durp
    
    Change-Id: Ibbe14064b1ea900db133885933b5b58716b4f445
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -37,6 +37,9 @@
 #include "msm_watchdog.h"
 #include "timer.h"
 #include "wdog_debug.h"
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <asm/kexec.h>
+#endif
 #ifdef CONFIG_LGE_HANDLE_PANIC
 #include <mach/lge_handle_panic.h>
 #include <mach/board_lge.h>
@@ -484,6 +487,26 @@ void msm_restart(char mode, const char *cmd)
 	printk(KERN_ERR "Restarting has failed\n");
 }
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+static void msm_kexec_hardboot_hook(void)
+{
+	set_dload_mode(0);
+
+	// Set PMIC to restart-on-poweroff
+	pm8xxx_reset_pwr_off(1);
+
+	// These are executed on normal reboot, but with kexec-hardboot,
+	// they reboot/panic the system immediately.
+#if 0
+	qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
+
+	/* Needed to bypass debug image on some chips */
+	msm_disable_wdog_debug();
+	halt_spmi_pmic_arbiter();
+#endif
+}
+#endif
+
 static int __init msm_pmic_restart_init(void)
 {
 	int rc;
@@ -531,6 +554,10 @@ static int __init msm_restart_init(void)
 
 #ifdef CONFIG_LGE_USE_DEFAULT_HARD_RESET
 	register_reboot_notifier(&msm_set_restart_reason_notifier);
+#endif
+
+#ifdef CONFIG_KEXEC_HARDBOOT
+	kexec_hardboot_hook = msm_kexec_hardboot_hook;
 #endif
 
 	return 0;
